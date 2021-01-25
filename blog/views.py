86f817ccb2d7b3359b3315554a8640c96d316e4c
Radi Obad <lovelovelove67@gmail.com>
2021-01-25 05:18:18 +0300

    Similar posts

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.db.models import Count
+
+from taggit.models import Tag
+
 from .models import Post
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -19,7 +29,8 @@ def post_list(request):
     return render(request,
                  'blog/post/list.html',
                  {'page': page,
-                  'posts': posts})
+                  'posts': posts,
+                  'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -28,9 +39,18 @@ def post_detail(request, year, month, day, post):
                                    publish__year=year,
                                    publish__month=month,
                                    publish__day=day)
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                .order_by('-same_tags','-publish')[:4]
+
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                  'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
